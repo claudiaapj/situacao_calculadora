6cb4e459a4db69e140a0b5b90e0531d50b987d9f
Cláudia Aguilar <aguilarp222@gmail.com>
2022-04-02 16:27:15 -0300

    Atualizando

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,11 @@ let valor2 = null
 let resultado = 0
 let labelVisor = document.querySelector('#visor')
 let operacao = ''
+let usePonto = false
+let onOff = true
+let onf = false
+let ativoNoturno = true
+
 
 // falta fazer o ponto e fazer funções para códigos repetidos
 function mudaVisor(){
@@ -26,123 +31,151 @@ function inicia(){
 }
 
 const on = function(){
-    if(visor !== null | valor1 !== null)
-    visor = '0'
+    onOff = false
+    if(onOff ===false){
+    visor = '0';
     valor1 = null;
     valor2 = null;
     resultado = ''
     mudaVisor()
+    }
 }
 
 const off = function(){
-    visor = ''
-    mudaVisor()
-    valor1 = null;
-    valor2 = null;
-    resultado = ''
-    //completar código não deixar ativar teclas
+    onOff = true
+    if(onOff === true){
+        visor = ''
+        mudaVisor()
+    }
 }
 
+
 const clickn7 = function(){
-    inicia()
-    visor = visor + '7';
-    mudaVisor()
+    if(onOff ===false){
+        inicia()
+        visor = visor + '7';
+        mudaVisor()
+    }
 }
 
 const clickn8 = function(){
+    if(onOff ===false){
     inicia()
     visor = visor + '8'
     mudaVisor()
-    
+    }
 }
 
 const clickn9 = function(){
-    inicia()
-    visor = visor + '9'
-    mudaVisor()     
+    if(onOff === false){
+        inicia()
+        visor = visor + '9';
+        mudaVisor()
+    }    
 }
 
 const clickn4 = function(){
-    inicia()
-    visor = visor + '4'
-    mudaVisor()     
+    if(onOff === false){
+        inicia()
+        visor = visor + '4';
+        mudaVisor()
+    }    
 }
 
 const clickn5 = function(){
-    inicia()
-    visor = visor + '5'
-    mudaVisor()     
+    if(onOff === false){
+        inicia()
+        visor = visor + '5';
+        mudaVisor()
+    }     
 }
 
 const clickn6 = function(){
-    inicia()
-    visor = visor + '6'
-    mudaVisor()    
+    if(onOff === false){
+        inicia()
+        visor = visor + '6';
+        mudaVisor()
+    }   
 }
 
 
 const clickn1 = function(){
-    inicia()
-    visor = visor + '1'
-    mudaVisor()    
+    if(onOff === false){
+        inicia()
+        visor = visor + '1';
+        mudaVisor()
+    }    
 }
 
 const clickn2 = function(){
-    inicia()
-    visor = visor + '2'
-    mudaVisor()    
+    if(onOff === false){
+        inicia()
+        visor = visor + '2';
+        mudaVisor()
+    }    
 }
 
 const clickn3 = function(){
-    inicia()
-    visor = visor + '3'
-    mudaVisor()
+    if(onOff === false){
+        inicia()
+        visor = visor + '3';
+        mudaVisor()
+    }
 }
 
 const clickn0 = function(){
-    inicia()
-    visor = visor + '0'
-    mudaVisor()
+    if(onOff === false){
+        inicia()
+        visor = visor + '0';
+        mudaVisor()
+    }
 }
 
-const clickOpSom = function(){
-    operacao = 'soma' 
+const ponto = function(){
+    if(onOff === false){
+        if(!usePonto){
+            visor = visor + '.'
+        }
+            usePonto= true
+    }
+
+}
+
+/*function modoNoturno(){
+    ativaNoturno = !ativaNoturno
+    if(ativaNoturno){
+        document.documentElement.style.setProperty(--color-bg-footer)
+    }
+}*/
+
+function condicao(){
     if(valor1===null && visor !== '') {
         let numero = parseFloat(visor)
         valor1 = numero 
         console.log('valor1 ' + valor1);
     } 
     visor = ''
+}
+
+
+const clickOpSom = function(){
+    operacao = 'soma' 
+    condicao()
 }
 
 const clickOpSub = function(){
     operacao = 'subtracao' 
-    if(valor1===null && visor !== '') {
-        let numero = parseFloat(visor)
-        valor1 = numero 
-        console.log('valor1 ' + valor1);
-    } 
-    visor = ''
+    condicao()
 }
 
 const clickOpMult = function(){
     operacao = 'multiplicacao' 
-    if(valor1===null && visor !== '') {
-        let numero = parseFloat(visor)
-        valor1 = numero 
-        console.log('valor1 ' + valor1);
-    } 
-    visor = ''
+    condicao()
 }
 
 const clickOpDiv = function(){
     operacao = 'divisao' 
-    if(valor1===null && visor !== '') {
-        let numero = parseFloat(visor)
-        valor1 = numero 
-        console.log('valor1 ' + valor1);
-    } 
-    visor = ''
+    condicao()
 }
 
 const conta = function (num1, num2) {
